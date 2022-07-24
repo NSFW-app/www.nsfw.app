1a6884028b2d0af4887b1dc95e5f197049fc1dec
x43n <x43n@pm.me>
2022-07-24 11:17:54 +1000

    fix: Cleanup Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -13,61 +13,65 @@ import {
 import { FOOTER_LINKS, FOOTER_SOCIAL_LINKS } from 'config'
 import { Link } from 'components/Link'
 
+const SocialsList = Stitches.styled(List, {
+  display: 'flex',
+  justifyContent: 'flex-end',
+  marginRight: '16px',
+  gap: '15px',
+  width: 'auto',
+  padding: '10px',
+  '@lg': {
+    width: '220px',
+  },
+})
+
+const CopyRightText = Stitches.styled(Text, {
+  fontSize: '11px',
+  lineHeight: '16px',
+  color: '$gray800',
+  display: 'none',
+  '@lg': {
+    display: 'block',
+  },
+})
+
+const FooterInnerContainer = Stitches.styled(Flex, {
+  height: '64px',
+  display: 'flex',
+  alignItems: 'center',
+  width: '100%',
+  transition: 'all 200ms ease',
+  justifyContent: 'space-between',
+  '@md': {
+    padding: '0 40px',
+  },
+  '@xxl': {
+    padding: '0 calc(50vw - 664px)',
+  },
+})
+
+const MarketingFooter = Stitches.styled(FooterPrimitive, {
+  backgroundColor: 'gray500',
+  borderTop: `1px solid $gray400`,
+  width: '100%',
+})
+
 export interface FooterProps
   extends Stitches.ComponentProps<typeof FooterPrimitive> {}
 
 export const Footer: React.FC<FooterProps> = ({ css }) => {
   return (
-    <FooterPrimitive
+    <MarketingFooter
       css={{
-        backgroundColor: Stitches.theme.colors.gray500,
-        borderTop: `1px solid ${Stitches.theme.colors.gray400}`,
-        width: '100%',
         ...css,
       }}
     >
-      <Flex
-        css={{
-          height: '64px',
-          display: 'flex',
-          alignItems: 'center',
-          width: '100%',
-          transition: 'all 200ms ease',
-          justifyContent: 'space-between',
-          '@md': {
-            padding: '0 40px',
-          },
-          '@xxl': {
-            padding: '0 calc(50vw - 664px)',
-          },
-        }}
-      >
+      <FooterInnerContainer>
         {' '}
-        <Text
-          css={{
-            fontSize: '11px',
-            lineHeight: '16px',
-            color: Stitches.theme.colors.gray800,
-
-            display: 'none',
-            '@lg': {
-              display: 'block',
-            },
-          }}
-        >
+        <CopyRightText>
           &copy; {new Date().getFullYear()} NSFW.app
-        </Text>
-        <List
-          horizontal
-          css={{
-            gap: '15px',
-            width: 'auto',
-            padding: '10px',
-            '@lg': {
-              width: '220px',
-            },
-          }}
-        >
+        </CopyRightText>
+        <SocialsList horizontal>
           {FOOTER_SOCIAL_LINKS.map(({ icon, href }) => (
             <Anchor
               key={href}
@@ -78,7 +82,7 @@ export const Footer: React.FC<FooterProps> = ({ css }) => {
               <Icon icon={icon} css={{ height: '22px', width: '22px' }} />
             </Anchor>
           ))}
-        </List>
+        </SocialsList>
         <Nav>
           <List horizontal css={{ gap: '15px' }}>
             {FOOTER_LINKS.map(({ name, href }) => (
@@ -97,7 +101,7 @@ export const Footer: React.FC<FooterProps> = ({ css }) => {
             ))}
           </List>
         </Nav>
-      </Flex>
-    </FooterPrimitive>
+      </FooterInnerContainer>
+    </MarketingFooter>
   )
 }
