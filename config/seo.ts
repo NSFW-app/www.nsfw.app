bad3b499a89fd1b47c136953debad0af24e7da4d
x43n <x43n@pm.me>
2022-07-28 19:36:08 +1000

    fix: Update meta

--- config/seo.ts
+++ config/seo.ts
@@ -3,6 +3,6 @@
 // SEO CONFIG
 export const META_TITLE_SUFFIX = ' | NSFW.app'
 
-export const DEFAULT_META_TITLE = `NSFW.app | The World's Leading Decentralized Adult Content Sharing Platform.`
+export const DEFAULT_META_TITLE = `NSFW.app | Crypto native adult platform`
 
-export const DEFAULT_META_DESC = `Our NSFW.app platform is the first of its kind. We are combining the likes of PornHub and OnlyFans onto a secure blockchain, to provide a revolutionary adult content sharing service, free from the usual issues surrounding traditional online adult entertainment platforms.`
+export const DEFAULT_META_DESC = `Crpto native adult content platform`
