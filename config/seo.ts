d26695c377739c195a8a73397c2cbfe038e7e956
x43n <x43n@pm.me>
2022-07-28 19:37:17 +1000

    fix: Meta

--- config/seo.ts
+++ config/seo.ts
@@ -5,4 +5,4 @@ export const META_TITLE_SUFFIX = ' | NSFW.app'
 
 export const DEFAULT_META_TITLE = `NSFW.app | Crypto native adult platform`
 
-export const DEFAULT_META_DESC = `Crpto native adult content platform`
+export const DEFAULT_META_DESC = `Crypto native adult content platform`
