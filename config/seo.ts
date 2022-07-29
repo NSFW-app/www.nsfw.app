ce551bf0a5de0df2a227c2f3b44846fce6c1b264
x43n <x43n@pm.me>
2022-07-30 07:27:37 +1000

    fix: Update seo

--- config/seo.ts
+++ config/seo.ts
@@ -5,4 +5,4 @@ export const META_TITLE_SUFFIX = ' | NSFW.app'
 
 export const DEFAULT_META_TITLE = `NSFW.app | Crypto native adult platform`
 
-export const DEFAULT_META_DESC = `Crypto native adult content platform`
+export const DEFAULT_META_DESC = `NSFW is a crypto native adult platform operated by a DAO. Engage with verified creators and join our welcoming community to explore your xxx side.`
