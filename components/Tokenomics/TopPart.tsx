081437f325c93be60f25ad4debaa9eb0238e9af5
Mihail <lepadatu.mihail1@gmail.com>
2022-10-27 15:30:31 +0200

    fix: Remove duplicate icons

--- components/Tokenomics/TopPart.tsx
+++ components/Tokenomics/TopPart.tsx
@@ -3,10 +3,8 @@ import { Heading, Text } from 'components/Typography'
 import { spaces } from 'config'
 import { GridContent, GridSection } from 'components/GridLayout'
 import { Link } from 'components/Link'
-import { VectorPurple } from 'components/Icons/VectorPurple'
-import { VectorPink } from 'components/Icons/VectorPink'
-import { VectorYellow } from 'components/Icons/VectorYellow'
 import { InnerGrid } from 'components/About/components/Block/StructureBlocks'
+import { BlopGreen } from 'components/Icons/Blops/BlopGreen'
 
 const FlexLayout = Stitches.styled(Flex, {
   gap: '40px',
@@ -27,7 +25,7 @@ export const TopPart: React.FC = ({}) => {
     <GridSection>
       <GridContent
         columns={{
-          tablet: '1 / 12',
+          tablet: '2 / 12',
           desktop: '1 / 24',
         }}
       >
@@ -82,7 +80,7 @@ export const TopPart: React.FC = ({}) => {
                 </Link>
               </Box>
             </Flex>
-            <Flex
+            {/* <Flex
               css={{
                 width: '100px',
                 position: 'relative',
@@ -91,21 +89,8 @@ export const TopPart: React.FC = ({}) => {
                 '@lg': { display: 'flex' },
               }}
             >
-              <VectorPink css={{ position: 'absolute', top: -190 }} />
-              <VectorPurple
-                css={{ position: 'absolute', left: 50, top: 250 }}
-              />
-              <VectorPurple css={{ position: 'absolute', left: 260 }} />
-              <VectorYellow
-                css={{ position: 'absolute', left: -50, bottom: 0 }}
-              />
-              <VectorYellow
-                css={{ position: 'absolute', left: 450, top: 100 }}
-              />
-              <VectorYellow
-                css={{ position: 'absolute', left: 310, top: 200 }}
-              />
-            </Flex>
+              <BlopGreen />
+            </Flex> */}
           </InnerGrid>
         </FlexLayout>
       </GridContent>
