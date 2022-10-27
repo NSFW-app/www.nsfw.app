e54b1c87f2322c87459bbd3a15d51d79dc5b3a62
Xaun Lopez <dev@xaunlopez.com>
2022-10-28 09:21:47 +1100

    fix: Diagonal break margin

--- components/DiagonalBgBreak/index.tsx
+++ components/DiagonalBgBreak/index.tsx
@@ -8,4 +8,7 @@ export const DiagonalBgBreak = Stitches.styled(Box, {
   borderWidth: '0 100vw 12vw 0',
   borderColor: '$gray500 $gray100 $gray500 $gray500',
   marginLeft: '-4px', // account for webkit scrollbar width
+  '@media (hover: none)': {
+    marginLeft: 0,
+  },
 })
